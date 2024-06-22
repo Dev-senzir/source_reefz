6fa687fa5989a9c253a088174add9a2838ac9aaa
DEVLOPER SENZIR <139072849+Dev-senzir@users.noreply.github.com>
2024-06-22 20:40:55 +0300

    Update server.py

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ api = Api(app)
 
 class Greeting (Resource):
     def get(self):
-        return "Tepthon is Up & Running!"
+        return "Refz is Up & Running!"
 
 api.add_resource(Greeting, '/')
 app.run(host="0.0.0.0", port=os.environ.get("PORT", 8080))
